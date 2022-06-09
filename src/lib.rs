fa296225f53d1fc54b7cfc0cd9aabaec18d88f3e
Windore <windore.wi@protonmail.com>
2022-06-09 17:10:47 +0300

    Move weekday_to_date tests to hide the func

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,7 @@ use chrono::{Date, Datelike, Local, Weekday};
 
 /// Gets the date that represents the upcoming weekday. Given tomorrow’s weekday, this should return
 /// tomorrows date. Today is represented by the current weekday.
-/// # Example
-/// ```
-/// use chrono::{Local, TimeZone, Weekday};
-/// use mtd::weekday_to_date;
-///
-/// // Today is a Tuesday
-/// let today = Local.ymd(2022,6,7);
-///
-/// // Tue should return today’s date
-/// assert_eq!(weekday_to_date(Weekday::Tue, today), today);
-///
-/// // Wed should return tomorrow’s date
-/// assert_eq!(weekday_to_date(Weekday::Wed, today), today.succ());
-///
-/// // Mon should return next weeks monday
-/// assert_eq!(weekday_to_date(Weekday::Mon, today), Local.ymd(2022,6,13));
-/// ```
-pub fn weekday_to_date(weekday: Weekday, mut today: Date<Local>) -> Date<Local> {
+fn weekday_to_date(weekday: Weekday, mut today: Date<Local>) -> Date<Local> {
     loop {
         if today.weekday() == weekday {
             return today;
@@ -53,4 +36,25 @@ impl Todo {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::weekday_to_date;
+
+    // Unit test a private function to remove the need to pass today into the Todo constructor
+    #[test]
+    fn weekday_to_date_returns_correct_dates() {
+        use chrono::{Local, TimeZone, Weekday};
+
+        // Today is a Tuesday
+        let today = Local.ymd(2022,6,7);
+
+        // Tue should return today’s date
+        assert_eq!(weekday_to_date(Weekday::Tue, today), today);
+
+        // Wed should return tomorrow’s date
+        assert_eq!(weekday_to_date(Weekday::Wed, today), today.succ());
+
+        // Mon should return next weeks monday
+        assert_eq!(weekday_to_date(Weekday::Mon, today), Local.ymd(2022,6,13));
+    }
+
+}
